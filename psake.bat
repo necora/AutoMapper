0f95b498fd2bc5208399b18141867d8558da3e06
tester <tester@.(none)>
2013-03-11 14:30:30 +0100

    deleted several files in plastic on root of the wk

--- psake.bat
+++
@@ -1,4 +0,0 @@
-powershell.exe -NoProfile -ExecutionPolicy unrestricted -Command "& {Import-Module '.\tools\psake\psake.psm1'; invoke-psake .\default.ps1 %*; if ($lastexitcode -ne 0) {write-host "ERROR: $lastexitcode" -fore RED; exit $lastexitcode} }" 
-
-
-
